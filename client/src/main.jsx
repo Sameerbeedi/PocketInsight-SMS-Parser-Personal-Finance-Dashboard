4f532f9ade84d6dd2101db17f5f38acb9c78bb1b
Sameerbeedi <beedi.sameer@gmail.com>
2025-11-18 19:10:08 +0530

    chore(debug): add console.log to main.jsx to confirm module execution

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
+import { AppDataProvider } from './context/AppDataContext.jsx';
+
+// Debugging helper: confirm this module executes in the browser
+console.log('[client] main.jsx executing');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <AppDataProvider>
+          <App />
+        </AppDataProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
